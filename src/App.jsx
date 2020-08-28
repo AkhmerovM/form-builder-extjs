015b0193dcbd69f646498c470936a888d1da19e7
Akhmerov <wertfani@mail.ru>
2020-08-28 21:14:16 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// @flow
+import React from 'react';
+
+export function App() {
+    return (
+        <div>some text</div>
+    );
+}
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { App } from './App';
+import './style.less';
+
+const root = document.getElementById('root');
+
+if (!root) {
+    throw new Error('root element not found');
+}
+ReactDom.render(<App />, root);
